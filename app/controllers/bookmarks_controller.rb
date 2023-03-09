6aa3b50a7524d561f34847110e1f7c43c3ac329d
BingYou Leong <byleong1990@gmail.com>
2023-03-09 17:01:22 +0800

    added bookmark html with links

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,29 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to bookmark_path(@bookmark)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "lists", to: "lists#index"
-  get "lists/new", to: "lists#new",  as: :new_list
-  post "lists", to: "lists#create"
-  get "lists/:id", to: "lists#show", as: :list
+  # get "lists", to: "lists#index"
+  # get "lists/new", to: "lists#new",  as: :new_list
+  # post "lists", to: "lists#create"
+  # get "lists/:id", to: "lists#show", as: :list
+  # get "list/:id/bookmarks/new", to: "bookmarks#new"
+  # post "list/:id/bookmarks", to: "bookmarks#create"
+  # delete "list/:id/bookmarks/:id", to: "bookmarks#destroy"
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create, :destroy]
+  end
 end

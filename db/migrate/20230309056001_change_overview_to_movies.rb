75212311478fc9181210817ab8bd6d9e36d494d6
BingYou Leong <byleong1990@gmail.com>
2023-03-09 15:23:16 +0800

    changed overview to text

--- db/migrate/20230309056001_change_overview_to_movies.rb
+++ db/migrate/20230309056001_change_overview_to_movies.rb
@@ -1,4 +1,4 @@
-class ChangeCommentToBookmarks < ActiveRecord::Migration[7.0]
+class ChangeOverviewToMovies < ActiveRecord::Migration[7.0]
   def change
     change_column(:movies, :overview, :text)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_056000) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_056001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,7 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_056000) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
-    t.string "overview"
+    t.text "overview"
     t.string "poster_url"
     t.integer "rating"
     t.datetime "created_at", null: false

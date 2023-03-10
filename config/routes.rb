9b8533dccc66bfdee4ce4d62121bd5ab11778151
BingYou Leong <byleong1990@gmail.com>
2023-03-10 13:01:52 +0800

    changed index to root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "lists#index", as: :lists
   # get "lists", to: "lists#index"
   # get "lists/new", to: "lists#new",  as: :new_list
   # post "lists", to: "lists#create"
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # get "list/:id/bookmarks/new", to: "bookmarks#new"
   # post "list/:id/bookmarks", to: "bookmarks#create"
   # delete "list/:id/bookmarks/:id", to: "bookmarks#destroy"
-  resources :lists, only: [:index, :new, :create, :show] do
+  resources :lists, only: [:new, :create, :show] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
